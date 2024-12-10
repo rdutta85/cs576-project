496fdcba69e2aad53a7d20fd399447787fe0bfa5
brtan8 <167389455+brtan8@users.noreply.github.com>
2024-12-10 12:37:56 -0500

    Added Scripts for transitioning between scenes

---
+++ Chord Strike/Assets/Scripts/Main Menu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button tutorial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button1.onClick.AddListener(delegate { ProcessButtonInput(1); });
+        button2.onClick.AddListener(delegate { ProcessButtonInput(2); });
+        button3.onClick.AddListener(delegate { ProcessButtonInput(3); });
+        tutorial.onClick.AddListener(delegate { ProcessButtonInput(4); });
+    }
+
+    void ProcessButtonInput(int id)
+    {
+        if (id == 1)
+        {
+            Debug.Log("Button 1 clicked");
+            SceneManager.LoadScene("Scenes/Level 1");
+        }
+        if (id == 2)
+        {
+            Debug.Log("Button 2 clicked");
+            SceneManager.LoadScene("Scenes/Level 2");
+        }
+        if (id == 3)
+        {
+            Debug.Log("Button 3 clicked");
+            SceneManager.LoadScene("Scenes/Level 3");
+        }
+        if (id == 4) 
+        {
+            Debug.Log("Tutorial clicked");
+            SceneManager.LoadScene("Scenes/Level 0");
+        }
+    }
+}

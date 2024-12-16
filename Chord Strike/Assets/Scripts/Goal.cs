2171a79e317a2a860b53c84e1dc1959760336f22
brtan8 <167389455+brtan8@users.noreply.github.com>
2024-12-16 13:20:20 -0500

    Changed Goal to implementing healing in level 3

--- Chord Strike/Assets/Scripts/Goal.cs
+++ Chord Strike/Assets/Scripts/Goal.cs
@@ -8,32 +8,46 @@ public class Goal3 : MonoBehaviour
 {
     public Material red;
     public Material green;
-    public GameObject obj;
-    public int temp;
+    public GameObject obj;    
+    private GameObject player;
+    private int temp;
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        obj.GetComponent<MeshRenderer>().material = red;
+        // Finds Player and sets the healing pad as green
+        player = GameObject.Find("JunkoChan");
+        obj.GetComponent<MeshRenderer>().material = green;
         temp = 0;
+        time = 10f;
     }
 
     // Update is called once per frame
+    // When healing pad is green then the player can heal 10 health. 
+    // After healing, the pad turns red for 10 seconds and the player can not heal
     void Update()
     {
-        if (1 == 1) { // Change condition to boss is killed or amount of enemies defeated
-            obj.GetComponent<MeshRenderer>().material = green;
-            temp = 1;
+        if (temp == 1) 
+        {
+            time -= Time.deltaTime;
+            if (time < 0) 
+            {
+                obj.GetComponent<MeshRenderer>().material = green;
+                temp = 0;
+                //Debug.Log("Reset");
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "JunkoChan")
+        if (other.gameObject.name == "JunkoChan" && temp == 0 && player.GetComponent<JunkochanControl>().Health < 100)
         {
-            if (temp == 1) {
-                SceneManager.LoadScene("Scenes/Menu");
-                temp = 0;
-            }
+            player.GetComponent<JunkochanControl>().Health += 20;
+            obj.GetComponent<MeshRenderer>().material = red;
+            temp = 1;
+            //Debug.Log("Healed");
+            time = 10f;
         }
     }
 }
